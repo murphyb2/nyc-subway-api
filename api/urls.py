e6afa800c63f9b8871cfa8048dfe7be7840c09bd
murphyb2 <32982028+murphyb2@users.noreply.github.com>
2021-04-20 10:59:53 -0400

    no trailing slash in urls

--- api/urls.py
+++ api/urls.py
@@ -4,15 +4,15 @@ from rest_framework.routers import DefaultRouter
 
 app_name = 'api'
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 urlpatterns = [
-	path('stations/', stations, name="stations"),
-    path('year/2021/', Yearly2021View.as_view()),
-    path('year/2020/', Yearly2020View.as_view()),
-    path('year/2019/', Yearly2019View.as_view()),
-    path('month/2021/', Monthly2021View.as_view()),
-    path('month/2020/', Monthly2020View.as_view()),
-    path('month/2019/', Monthly2019View.as_view()),
+	path('stations', stations, name="stations"),
+    path('year/2021', Yearly2021View.as_view()),
+    path('year/2020', Yearly2020View.as_view()),
+    path('year/2019', Yearly2019View.as_view()),
+    path('month/2021', Monthly2021View.as_view()),
+    path('month/2020', Monthly2020View.as_view()),
+    path('month/2019', Monthly2019View.as_view()),
     path('totals', AggregateView.as_view()),
     path('update', WeeklyUpdateView.as_view()),
 ]
