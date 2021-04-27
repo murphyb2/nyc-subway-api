ed377111d91e7c6714ad91ac443c5331d7cbf8e8
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2021-04-27 11:08:49 -0400

    static and media file config

--- subwayapi/settings.py
+++ subwayapi/settings.py
@@ -144,7 +144,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/subway/'
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # S3 Config
 if DEBUG is False:
