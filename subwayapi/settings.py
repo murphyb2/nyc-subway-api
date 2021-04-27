07a0e6ac3a88ae64f4748816ad556333d11d3d08
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2021-04-27 11:21:59 -0400

    set default acl to public-read

--- subwayapi/settings.py
+++ subwayapi/settings.py
@@ -154,7 +154,7 @@ if DEBUG is False:
     AWS_REGION=config("AWS_REGION")
     
     AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = None
+    AWS_DEFAULT_ACL = 'public-read'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
