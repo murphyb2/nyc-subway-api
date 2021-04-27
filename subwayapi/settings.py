6418531975862fde07bee08baa4f119ab1ab35c4
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2021-04-27 10:40:39 -0400

    s3 acl

--- subwayapi/settings.py
+++ subwayapi/settings.py
@@ -151,7 +151,9 @@ if DEBUG is False:
     AWS_SECRET_ACCESS_KEY=config("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME=config("AWS_STORAGE_BUCKET_NAME")
     AWS_REGION=config("AWS_REGION")
-
+    
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
