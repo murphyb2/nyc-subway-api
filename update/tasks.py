3e2941d3ca766e6dd2c432bea4bb380b582c6487
murphyb2-Gary <32982028+murphyb2@users.noreply.github.com>
2021-04-27 16:13:15 -0400

    add weekly update task

--- update/tasks.py
+++ update/tasks.py
@@ -11,38 +11,38 @@ def add(x,y):
     print(f'adding {x} and {y}')
     return x + y
 
-# @shared_task
-# def WeeklyUpdate():        
-#     upToDate = shouldUpdate()
-#     if "error" in upToDate:
-#         return Response({'success': False, 'error': upToDate["error"]})
+@shared_task
+def WeeklyUpdate():        
+    upToDate = shouldUpdate()
+    if "error" in upToDate:
+        return {'success': False, 'error': upToDate["error"]}
 
-#     if(upToDate["shouldUpdate"] == False):
-#         return Response({'success': True, 'message': "All records are up to date"})
+    if(upToDate["shouldUpdate"] == False):
+        return {'success': True, 'message': "All records are up to date"}
     
-#     print("db most recent saturday: " + upToDate["dbMostRecentSaturday"].strftime("%Y-%m-%d"))
-#     print("current saturday: " + upToDate["currentSaturday"].strftime("%Y-%m-%d"))
+    print("db most recent saturday: " + upToDate["dbMostRecentSaturday"].strftime("%Y-%m-%d"))
+    print("current saturday: " + upToDate["currentSaturday"].strftime("%Y-%m-%d"))
 
-#     saturdayToImport = upToDate["dbMostRecentSaturday"] + timedelta(days=7) 
-#     currentSaturday = upToDate["currentSaturday"]
+    saturdayToImport = upToDate["dbMostRecentSaturday"] + timedelta(days=7) 
+    currentSaturday = upToDate["currentSaturday"]
 
-#     # download and import new weekly data
-#     while(saturdayToImport <= currentSaturday):
-#         result = importCSV(DATE=saturdayToImport.strftime("%y%m%d"))
-#         if(result['success'] == False):
-#             content = {
-#                 'success': result['success'],
-#                 'error': result['error']
-#             }
-#             return Response(content, status=result['status'])   
-#         saturdayToImport = saturdayToImport + timedelta(days=7)  
+    # download and import new weekly data
+    while(saturdayToImport <= currentSaturday):
+        result = importCSV(DATE=saturdayToImport.strftime("%y%m%d"))
+        if(result['success'] == False):
+            content = {
+                'success': result['success'],
+                'error': result['error']
+            }
+            return content
+        saturdayToImport = saturdayToImport + timedelta(days=7)  
 
-#     # update net values
-#     result = updateValues(year = upToDate["dbMostRecentSaturday"].year)
-#     content = {
-#         'success': result['success'],
-#         'error': result['error']
-#     }
+    # update net values
+    result = updateValues(year = upToDate["dbMostRecentSaturday"].year)
+    content = {
+        'success': result['success'],
+        'error': result['error']
+    }
 
-#     return Response(content, status=result['status'])
+    return content
 
